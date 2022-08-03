925b9d92cf7c2884bfa7f1b10a19d570759876fc
kavyasudhakar <kavyasudhakar068@gmail.com>
2022-08-03 14:51:06 +0530

    added devise and admin,user functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :words
+  
+  devise_for :users
+  root 'home#index'
+  get "users", to: "devise/session#new"
+  
 end
